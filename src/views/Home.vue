51910613ca6794ea2e9e55a07b430e8c352033bc
matthew-hub <mateuszr430@gmail.com>
2021-04-09 09:35:41 +0200

    create home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="app-content"></div>
+  <div class="home-content">
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import AppHeader from '@/components/layouts/AppHeader.vue';
+import AppMain from '@/components/layouts/AppMain.vue';
+import AppFooter from '../components/layouts/AppFooter.vue';
 
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.home-content {
+  padding: 20px;
+  min-height: 100%;
+  display: grid;
+  grid-gap: 10px;
+  background: linear-gradient(45deg, #010101 0%, #101010 40%, #101a1d 100%);
+  background-repeat: no-repeat;
+  grid-template-columns: minmax(335px, 800px);
+  grid-template-areas: 'nav' 'main' 'footer';
+  grid-template-rows: auto 1fr auto;
+  justify-content: center;
+}
+</style>
