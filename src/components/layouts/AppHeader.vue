041eb3cb7de29b7f7d6577d1c0e10d740f0b066e
matthew-hub <mateuszr430@gmail.com>
2021-04-14 00:46:19 +0200

    handle input

--- src/components/layouts/AppHeader.vue
+++ src/components/layouts/AppHeader.vue
@@ -2,7 +2,16 @@
   <header class="app-header">
     <AppTitle />
     <AppInfo />
-    <SearchBar />
+    <SearchBar>
+      <input
+        v-model.trim="steamInput"
+        type="text"
+        class="input-bar__search"
+        spellcheck="false"
+        placeholder="Your profile url, vanityid or steamid"
+        @keyup.enter="handleInput"
+      />
+    </SearchBar>
   </header>
 </template>
 
@@ -10,13 +19,37 @@
 import AppTitle from '@/components/AppTitle.vue';
 import AppInfo from '@/components/AppInfo.vue';
 import SearchBar from '@/components/SearchBar.vue';
+import steamAPI from '@/API/steam-api';
 
 export default {
   name: 'AppHeader',
+  data() {
+    return {
+      steamInput: ''
+    };
+  },
   components: {
     AppTitle,
     AppInfo,
     SearchBar
+  },
+
+  methods: {
+    handleInput(e) {
+      e.preventDefault();
+      e.target.blur();
+      // e.targe.blur = 'none';
+      const inputData = steamAPI.parseInputData(this.steamInput);
+      console.log('[inputData]:', inputData);
+
+      this.$emit('emitHandleInput', inputData);
+      // this.steamInput = '';
+    },
+    sendInput() {
+      console.log('[aaa]:', this.steamInput);
+
+      this.$emit('created', this.steamInput);
+    }
   }
 };
 </script>
@@ -27,7 +60,8 @@ export default {
   display: grid;
   grid-template-columns: auto;
   grid-template-rows: 1fr auto auto;
-  padding-top: 150px;
+  padding-top: 20%;
+  // padding-top: 160px;
   justify-items: center;
 }
 </style>
